e6e972aa6df076b085ffce96291d277058c90fef
marwaaboshall <marwa.aboshall@gmail.com>
2018-09-16 14:30:30 +0200

    Retrieve questions from polls API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    questions: []
+  };
+  componentDidMount() {
+    fetch('https://polls.apiblueprint.org/questions?page=1')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.setState({ questions: data });
+      console.log(data);
+    })
+    .catch((err) =>{
+      console.log('An error happened fetching the data' + err);
+    });
+  }
   render() {
     return (
       <div>
